aab9582246898b5b97acdd82fce8a734e2e42158
Gilvan <camisetaecia@live.com>
2019-10-05 11:21:46 -0300

    Http Response dealings

--- src/main/java/com/tinellus/socialbooks/resources/LivrosResources.java
+++ src/main/java/com/tinellus/socialbooks/resources/LivrosResources.java
@@ -1,14 +1,17 @@
 package com.tinellus.socialbooks.resources;
 
-import java.util.ArrayList;
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.tinellus.socialbooks.domain.Livro;
 import com.tinellus.socialbooks.repository.LivrosRepository;
@@ -38,13 +41,21 @@ public class LivrosResources {
 	} //end method to list
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public void savar(@RequestBody Livro livro) {
-		livrosRepository.save(livro);
+	public ResponseEntity<Void> savar(@RequestBody Livro livro) {
+		livro = livrosRepository.save(livro);
+		 URI uri  = ServletUriComponentsBuilder.fromCurrentRequest()
+				 .path("/{id}").buildAndExpand(livro.getId()).toUri();
+		   return ResponseEntity.created(uri).build();
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public Livro buscar(@PathVariable("id") Long id) {
-		return livrosRepository.findById(id).orElse(null);
+	public ResponseEntity<?> buscar(@PathVariable("id") Long id) {
+		Livro livro =  livrosRepository.findById(id).orElse(null);
+		if(livro == null) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		return ResponseEntity.status(HttpStatus.OK).body(livro);
 	}
 	
 	@RequestMapping(value = "/{id}",method = RequestMethod.DELETE)
